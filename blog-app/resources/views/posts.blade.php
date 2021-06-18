767fb841b42f25c80012b19970c58a42aa590bcc
yasu <rock.fish.junkie@gmail.com>
2021-06-18 14:36:50 +0900

    laracat ep10

---
+++ blog-app/resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my blog</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p> from now,I plactice laravel & clreate my Apps!</p>
+    </article>
+  
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p> My fishing is very funny!</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p> Study is very funny!</p>
+    </article>
+
+</body>
+</html>
--- blog-app/routes/web.php
+++ blog-app/routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+
+Route::get('posts/{post}',function($slug){
+
+    $path=__DIR__."/../resources/posts/{$slug}.html";
+    // ddd($path);
+
+    if(! file_exists($path)){
+        return redirect('/');
+    }
+    
+    $post = cache()->remember("posts.{$slug}",5,function() use($path){
+      
+        var_dump('file_get_contents');
+        return file_get_contents($path);
+
+    });
+
+    return view('post',[
+        'post'=>$post,
+    ]);
+})->where('post','[A-z_\-]+');
