b3363b3150459c5f9ac710414f579b81276b4fb0
yasu <rock.fish.junkie@gmail.com>
2021-07-21 00:47:17 +0900

    dataへ

--- resources/views/fishing-app.blade.php
+++ resources/views/fishing-app.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
        
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Fishing App') }}
+            {{ __('Fishing Data') }}
         </h2>
        
     </x-slot>
