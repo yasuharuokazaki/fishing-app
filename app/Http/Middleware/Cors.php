c97463727854d31402bc515b534e1f7ec14b95c3
yasu <rock.fish.junkie@gmail.com>
2021-07-24 14:28:08 +0900

    kernel stop

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,10 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
+        return $next($request);
                     // クロスオリジンを許可するオリジンを指定
-                    ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080')
-                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                    ->header('Access-Control-Allow-Headers', 'Content-Type');
+                    
     }
 }
